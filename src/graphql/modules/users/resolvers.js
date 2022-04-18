76c6ae573745ba6d019f90e60d825b1eab98a10f
Felipe <fcalldas@gmail.com>
2022-04-18 11:15:27 -0300

    🎨 formating the code

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,12 +1,9 @@
 
 export default {
     Query: {
-
         getUser: async (parent, { id }, { database }, info) => {
             return await database.user.findByPk(id)
         },
-
-
         getAllUsers: async (parent, _, { database }, info) => {
             return await database.user.findAll()
         }
@@ -15,7 +12,7 @@ export default {
         createUser: async (parent, { input }, { database }, info) => {
             const user = await database.user.create({
                 fullname: input.fullname,
-                email: input.email,
+                email: input.email
             })
             return user;
         },
@@ -31,9 +28,7 @@ export default {
             const deleteUser = await user.update({
                 removed_at: new Date()
             });
-
             return !!deleteUser;
         }
-
     },
 };
