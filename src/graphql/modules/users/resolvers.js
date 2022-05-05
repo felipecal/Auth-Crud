37015fa41142e7b69c27fb1104e834fb942a8511
Felipe <fcalldas@gmail.com>
2022-05-05 16:05:02 -0300

    ✨  new features

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,26 +1,23 @@
 import sequelize from 'sequelize';
 export default {
-    User: {
-        full_name: (user) => `${user.first_name} ${user.last_name}`
-    },
     Query: {
         getUser: async (parent, { id }, { database }, info) => {
             return await database.user.findByPk(id)
         },
-        getAllUsers: async (parent,_, { database }, info) => {
+        getAllUsers: async (parent, _, { database }, info) => {
             return await database.user.findAll()
         },
         getNotRemovedUsers: async (parent, { id }, { database }, info) => {
             const Op = sequelize.Op;
-            return await database .user.findAll({
-                where: { 
-                    removed_at: { 
-                        [Op.eq]: null, 
+            return await database.user.findAll({
+                where: {
+                    removed_at: {
+                        [Op.eq]: null,
                     }
                 }
             })
         },
-        getRemovedUsers: async (parent,_, { database }, info) => {
+        getRemovedUsers: async (parent, _, { database }, info) => {
             const Op = sequelize.Op;
             return await database.user.findAll({
                 where: {
@@ -58,17 +55,20 @@ export default {
             if (!user) throw new Error('User not found');
             const deleteUser = await user.update({
                 removed_at: new Date()
-            });
+            }); 
             return !!deleteUser;
         }
     },
     User: {
-        post: async (parent, { database }, info) => {
-            const post = await database.post.findAll();
+        post: async (parent, args, { database }, info) => {
+            const post = await database.post.findAll({ where: { 
+                user_id: parent.id
+            }});
             if (!post) {
                 throw new Error('Post not found');
             }
             return post;
-    },
+        },
+        full_name: (user) => `${user.first_name} ${user.last_name}`
     }
 }
